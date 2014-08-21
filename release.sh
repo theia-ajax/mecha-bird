27f6f6c0406754e2387128c56345df2299ae455c
Ted Dobyns <ted@tedajax.net>
2014-08-20 17:33:31 -0700

    dope release script

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-SERVER_USER = ted
-SERVER_ADDRESS = 173.255.220.111
-TARGET_DIR = ../
-TARGET = mecha-prototype.love
+SERVER_USER="ted"
+SERVER_ADDRESS="173.255.220.111"
+TARGET_DIR=".."
+TARGET="mecha-prototype.love"
 
-git archive --format zip --output $(TARGET_DIR)$(TARGET) master
-scp $(TARGET_DIR)($TARGET) $(SERVER_USER)@$(SERVER_ADDRESS):~/$(TARGET)
+git archive --format zip --output $TARGET_DIR/$TARGET master
+scp $TARGET_DIR/$TARGET $SERVER_USER@$SERVER_ADDRESS:~/$TARGET
